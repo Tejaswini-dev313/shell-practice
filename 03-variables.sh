573c28da96ce4e716f80235eee44a08b55a412a6
Tejaswini-dev313 <bodapatlatejaswini1@gamil.com>
2024-12-21 10:20:57 +0530

    Added variables to conversation

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Person1=Ramesh
+Person2=Suresh
+
+echo "$Person1:: Hi Suresh, How are you?"
+echo "{$Person2}:: Hello $Person1, I am fine. How are you doing?"
+echo "$Person1:: I am doing good, what are you doing?"
+echo "${Person2}:: I am learing shell script $Person1"
