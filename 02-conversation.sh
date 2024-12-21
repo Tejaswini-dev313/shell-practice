5b59c60aab74cb60bd35dd5afd166eec31e5d568
Tejaswini-dev313 <bodapatlatejaswini1@gamil.com>
2024-12-21 10:11:42 +0530

    added conversation

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: Hi Suresh, How are you?"
+echo "Suresh:: Hello Ramesh, I am fine. How are you doing?"
+echo "Ramesh:: I am doing good, what are you doing?"
+echo "Suresh:: I am learing shell script ramesh"
