37ca606a7a26e9cf7a54e2f10baf1f5c0f86ac67
Tejaswini-dev313 <bodapatlatejaswini1@gamil.com>
2024-12-21 10:26:27 +0530

    variable changes

--- 03-variables.sh
+++ 03-variables.sh
@@ -4,6 +4,6 @@ Person1=Ramesh
 Person2=Suresh
 
 echo "$Person1:: Hi Suresh, How are you?"
-echo "{$Person2}:: Hello $Person1, I am fine. How are you doing?"
+echo "${Person2}:: Hello $Person1, I am fine. How are you doing?"
 echo "$Person1:: I am doing good, what are you doing?"
 echo "${Person2}:: I am learing shell script $Person1"
