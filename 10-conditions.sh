6677d57fe0e3f48fd1d2cccabfc965a2f2d099f7
Tejaswini-dev313 <bodapatlatejaswini1@gamil.com>
2025-01-04 19:28:53 +0530

    conditions

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -2,10 +2,10 @@
 
 NUMBER1=$50
 
-if [ $NUMBER1 -gt 20 ]  #gt, lt, eq, -ne
+if [ $NUMBER1 -gt 100 ]  #gt, lt, eq, -ne
 then
-    echo "Given number: $NUMBER1 is greater than 20"
+    echo "Given number: $NUMBER1 is greater than 100"
 else
-    echo "Given number: $NUMBER1 is less than 20"
+    echo "Given number: $NUMBER1 is less than 100"
 fi
 
