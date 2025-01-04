0cb4dca17e9aabc6f86a0989f1fbe4145ef5bf9a
Tejaswini-dev313 <bodapatlatejaswini1@gamil.com>
2025-01-04 13:11:32 +0530

    special variables

--- 09-special-variables.sh
+++ 09-special-variables.sh
@@ -5,3 +5,5 @@ echo "all variables passed to the script: $@"
 echo "how many varialbes passed to the script: $#"
 
 echo "what is the current working directory: $PWD"
+
+echo "what is the current script name: $0"
