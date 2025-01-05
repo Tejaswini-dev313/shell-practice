bae98a3e7344df3b49fc73e2aa5acf0bc01ccf17
Tejaswini-dev313 <bodapatlatejaswini1@gamil.com>
2025-01-05 10:44:42 +0530

    installation

--- 11-installation.sh
+++ 11-installation.sh
@@ -10,3 +10,13 @@ then
     exit 1
 fi
 
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then 
+    echo "Git installation is not success and check it"
+    dnf install git -y
+else
+    echo "Git is already installed"
+fi
