7625583172345a8cf52b69b9e593e41906b6a557
Tejaswini-dev313 <bodapatlatejaswini1@gamil.com>
2025-01-18 19:15:09 +0530

    practice

--- 14-practice.sh
+++ 14-practice.sh
@@ -3,8 +3,8 @@
 Person1=Ramesh
 Preson2=Suresh
 
-echo "$Person1:: Hi $Person2, How are you?"
-echo "$Person2:: Hello $Person1, I am fine. How are you doing?"
+echo "$Person1:: Hi ${Person2}, How are you?"
+echo "${Person2}:: Hello $Person1, I am fine. How are you doing?"
 echo "$Person1:: I am doing good, what are you doing?"
-echo "$Person2:: I am learing shell script $Person1"
+echo "${Person2}:: I am learing shell script $Person1"
 
