062caa61313d2114b3f02bacb1176a03d015f61e
Tejaswini-dev313 <bodapatlatejaswini1@gamil.com>
2025-01-23 12:19:54 +0530

    practice

--- 14-practiceshell.sh
+++ 14-practiceshell.sh
@@ -1,5 +1,12 @@
 #!bin/bash
 
-DATE=$(date +%c)
+# DATE=$(date +%c)
 
-echo "Today date is: $DATE"
+# echo "Today date is: $DATE"
+
+NUMBER1=$1
+NUMBER2=$2
+
+SUM=$(($NUMBER1+$NUMBER2))
+
+echo "Total $NUMBER1 and $NUMBER2 is: $SUM"
