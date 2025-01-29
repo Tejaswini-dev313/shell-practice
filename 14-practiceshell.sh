ab4fcfa0fa910b977dba657ae986e3bd2f41c9c4
Tejaswini-dev313 <bodapatlatejaswini1@gamil.com>
2025-01-29 19:36:55 +0530

    practice

--- 14-practiceshell.sh
+++ 14-practiceshell.sh
@@ -4,13 +4,25 @@
 
 # echo "Today date is: $DATE"
 
-NUMBER1=$1
-NUMBER2=$2
+# NUMBER1=$1
+# NUMBER2=$2
 
-SUM=$(($NUMBER1+$NUMBER2))
+# SUM=$(($NUMBER1+$NUMBER2))
 
-echo "Total $NUMBER1 and $NUMBER2 is: $SUM"
+# echo "Total $NUMBER1 and $NUMBER2 is: $SUM"
 
-sleep 100 &
+# sleep 100 &
 
-echo "PID of last background command: $!"
+# echo "PID of last background command: $!"
+
+
+NUMBER=$1
+
+if [ $NUMBER -gt 30 ]
+
+then
+    echo "Given number: $NUMBER is greater then 30"
+else
+    echo "Given number: $NUMBER is less than 30"
+fi
+
