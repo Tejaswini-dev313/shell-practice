12c71aa8d49d12f3e7a6e1d7095646fa69707cd9
Tejaswini-dev313 <bodapatlatejaswini1@gamil.com>
2025-03-08 18:34:01 +0530

    practice

--- 22-practiceshell.sh
+++ 22-practiceshell.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-$NUMBER1=$1
+NUMBER1=$1
 
-if (NUMBER -gt 30)
-than
+if ($NUMBER1 -gt 30)
+then
     echo "Given $NUMBER1 is greater than 30"
 else
     echo "Given $NUMBER1 is less than 30"
 fi
 
 
-
+# if [ $NUMBER1 -gt 20 ]  # gt, lt, eq, -ne
+# then
+#     echo "Given number: $NUMBER1 is greater than 20"
+# else
+#     echo "Given number: $NUMBER1 is less than 20"
+# fi
