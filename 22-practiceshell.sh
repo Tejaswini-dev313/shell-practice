186650a014ae0c430172f9f6bb7638d3e1c5446a
Tejaswini-dev313 <bodapatlatejaswini1@gamil.com>
2025-03-08 17:54:47 +0530

    practice

--- 22-practiceshell.sh
+++ 22-practiceshell.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+echo "Teju: Hii, How are you chinna"
+echo "chinna: Hello im fine Teju"
+echo "Teju: What are you doing"
+echo "chinna: nothing Teju, watching TV"
 
-echo "Hello world"
+
