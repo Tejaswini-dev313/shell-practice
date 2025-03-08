62107ea9ce27bd0009345192055acfe3d561d628
Tejaswini-dev313 <bodapatlatejaswini1@gamil.com>
2025-03-08 18:38:49 +0530

    practice

--- 22-practiceshell.sh
+++ 22-practiceshell.sh
@@ -2,17 +2,17 @@
 
 NUMBER1=$1
 
-# if [ $NUMBER1 -gt 30 ]
-# then
-#     echo "Given $NUMBER1 is greater than 30"
-# else
-#     echo "Given $NUMBER1 is less than 30"
-# fi
-
-
-if [ $NUMBER1 -gt 20 ]  # gt, lt, eq, -ne
+if [ $NUMBER1 -gt 30 ]
 then
-    echo "Given number: $NUMBER1 is greater than 20"
+    echo "Given $NUMBER1 is greater than 30"
 else
-    echo "Given number: $NUMBER1 is less than 20"
+    echo "Given $NUMBER1 is less than 30"
 fi
+
+
+# if [ $NUMBER1 -gt 20 ]  # gt, lt, eq, -ne
+# then
+#     echo "Given number: $NUMBER1 is greater than 20"
+# else
+#     echo "Given number: $NUMBER1 is less than 20"
+# fi
