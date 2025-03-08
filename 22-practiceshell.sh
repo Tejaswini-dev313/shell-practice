31464296e7084d92448dac081b8c4cac0fcb3175
Tejaswini-dev313 <bodapatlatejaswini1@gamil.com>
2025-03-08 18:12:37 +0530

    practice

--- 22-practiceshell.sh
+++ 22-practiceshell.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
+echo "Please enter your username::"
 
-Person1=$1
-Person2=$2
+read USERNAME
 
-echo "$Person1: Hii, How are you $Person2"
-echo "$Person2: Hello im fine $Person1"
-echo "$Person1: What are you doing"
-echo "$Person2: nothing $Person1, watching TV"
-
-
+echo "username entered is: $USERNAME"
