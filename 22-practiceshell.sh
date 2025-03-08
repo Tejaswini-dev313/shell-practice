761d71190fc66d9126a68a154df635e54cb763da
Tejaswini-dev313 <bodapatlatejaswini1@gamil.com>
2025-03-08 18:25:29 +0530

    practice

--- 22-practiceshell.sh
+++ 22-practiceshell.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-FRUITS=("1" "2" "3")
+DATE=$(date)
 
-echo "First fruit name is: ${FRUITS[0]}"
-echo "second fruit name is: ${FRUITS[1]}"
-echo "Third fruit name is: ${FRUITS[2]}"
-
-echo "Total fruit name is: ${FRUITS[@]}"
+echo "Today date is $DATE"
 
 
 
