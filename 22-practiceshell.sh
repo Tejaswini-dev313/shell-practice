ca6214f1c0a80e9604a1f4c4297d56865790abe9
Tejaswini-dev313 <bodapatlatejaswini1@gamil.com>
2025-03-08 18:17:06 +0530

    practice

--- 22-practiceshell.sh
+++ 22-practiceshell.sh
@@ -4,12 +4,12 @@ echo "Please enter your username::"
 
 read -s USERNAME
 
-echo "username entered is: $USERNAME"
+
 
 echo "Please enter password:"
 
 read -s PASSWORD
 
-echo "Password entered is: $PASSWORD"
+
 
 
