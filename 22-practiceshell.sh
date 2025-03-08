bd68693933a07a1dbb2c836f4212454df4a1ea20
Tejaswini-dev313 <bodapatlatejaswini1@gamil.com>
2025-03-08 18:45:24 +0530

    practice

--- 22-practiceshell.sh
+++ 22-practiceshell.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-NUMBER1=$1
+CLEAR=$(clear)
 
-if [ $NUMBER1 -lt 30 ]
-then 
-    echo "Given $NUMBER1 is less than 30"
-else
-    echo "Given $NUMBER1 is greater than 30"
-fi
+echo "Cleared the data: $CLEAR"
 
