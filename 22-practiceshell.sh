865d2c392ed38426ada4598da379eb86ffe87e82
Tejaswini-dev313 <bodapatlatejaswini1@gamil.com>
2025-03-09 20:12:49 +0530

    practice

--- 22-practiceshell.sh
+++ 22-practiceshell.sh
@@ -5,7 +5,23 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "Run the script with root previliges"
+    exit 1
 fi
 
 dnf install mysql -y
+
+if [ $? -ne 0 ]
+then
+    echo "mysql is not installed and going to install it.."
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then
+        echo "mysql installation is not successful..check it"
+        exit 1
+    else
+        echo "mysql is installed successful"
+    fi
+else
+    echo "mysql is already installed"
+fi
 
