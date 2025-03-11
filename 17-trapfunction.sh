ae56f1a0c693ba5be4343320c6936596ea20f8bc
Tejaswini-dev313 <bodapatlatejaswini1@gamil.com>
2025-03-11 09:36:03 +0530

    trapfunction

--- 17-trapfunction.sh
+++ 17-trapfunction.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+set -e # setting the automatic exit, if we get error and set -ex for debug
+
+failure(){
+    echo "Failed at: $1:$2"
+}
+
+trap 'failure "${LINENO}" "$BASH_COMMAND"' ERR #ERR is the error signal
+
 echo "Hello world success"
 echooo "Hello world success"
 echo "Hello world after failure"
