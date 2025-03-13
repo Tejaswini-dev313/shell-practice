c3a0ab8a1c1cb9f6638377f07a378a6285b3d149
Tejaswini-dev313 <bodapatlatejaswini1@gamil.com>
2025-03-13 20:21:30 +0530

    backup

--- 20-disk-usage.sh
+++ 20-disk-usage.sh
@@ -5,5 +5,10 @@ DISK_THRESHOLD=5
 
 while IFS= read -r line
 do
-    echo "$line"
+    USAGE=$(echo $line | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
+    PARTITION=$(echo $line | grep xfs | awk -F " " '{print $NF}')
+    if [ $USAGE -ge $DISK_THRESHOLD ]
+    then
+        echo "$PARTITION is more than $DISK_THRESHOLD, current value: $USAGE, please check"
+    fi
 done <<< $DISK_USAGE
