776989c1f3493e5e38a4f167285c09c43d90eaa4
Tejaswini-dev313 <bodapatlatejaswini1@gamil.com>
2025-03-13 14:24:40 +0530

    backup

--- 19-backup.sh
+++ 19-backup.sh
@@ -35,7 +35,7 @@ FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime +14)
 
 echo "Files: $FILES"
 
-if [ -n $FILES ] #true if FILES are empty, ! makes it expression false
+if [ ! -z $FILES ] #true if FILES are empty, ! makes it expression false
 then
     echo "Files are found"
 else   
